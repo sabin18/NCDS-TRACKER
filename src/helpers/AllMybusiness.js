5513231cb37483b0e99474535ade7ba65cf4a310
sabin18 <sabin6341@gmail.com>
2020-08-16 16:53:11 +0200

    fix mybusiness helper

--- src/helpers/AllMybusiness.js
+++ src/helpers/AllMybusiness.js
@@ -8,14 +8,12 @@ const AllMyPharmacy = async (req,res)  =>{
 const { id } = req.user.payload;
 
 const pharmacy = await models.pharmacy.findAll({where:{owner:id}});
-console.log('===>1',pharmacy.length)
 const employee = await models.employees.findOne({ where:{userId:id}});
 
 const allEmployeePharmacy =!employee ? employee: await models.pharmacy.findAll({where:{id:employee.pharmacyId},
     attributes: { exclude: ['payment'] },
     include: [{ association: 'user',attributes: { exclude: ['password','role','createdAt','updatedAt'] }},{ association: 'payments', attributes: ['amount','payDate','expiryDate','period'] }],
      })
-     console.log('===>2',allEmployeePharmacy)
  if(pharmacy.length===0 && allEmployeePharmacy.length===0){
    return  ErrorResponse(res,404,strings.pharmacy.error.pharmacy_NOT_EXIST);
  }
